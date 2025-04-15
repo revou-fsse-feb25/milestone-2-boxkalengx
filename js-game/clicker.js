99c34cbf368dace61d67201e5df26de51d04dfe9
rian arif rahman <boxkaleng@gmail.com>
2025-04-15 23:42:12 +0700

    add clicker game

---
+++ js-game/clicker.js
@@ -0,0 +1,16 @@
+let gem = document.querySelector(".gem-cost");
+let parsedGem = parseFloat(gem.innerHTML);
+
+let clickerCost = document.querySelector(".clicker-cost");
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+function incrementGem() {
+  parsedGem += 1;
+  gem.innerHTML = parsedGem;
+}
+
+function buyClicker() {
+  if (parsedGem >= parsedClickerCost) {
+    parsedGem -= parsedClickerCost;
+    gem.innerHTML = parsedGem;
+  }
+}
