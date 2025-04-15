923df4e368a7156456b6affea85bcd362c3ecca6
rian arif rahman <boxkaleng@gmail.com>
2025-04-16 00:49:17 +0700

    add some adjustment for each page

--- js-game/scissors.js
+++ js-game/scissors.js
@@ -8,10 +8,10 @@ function play(playerChoice) {
   if (playerChoice === computerChoice) {
     result = "It's a draw!";
   } else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
-    result = "You win!";
+    result = "you win, Nice Try!";
     score++;
   } else {
-    result = "You lose!";
+    result = "you lose mate!";
     score--;
   }
   document.getElementById("result").textContent = `Computer chose ${computerChoice}. ${result}`;
